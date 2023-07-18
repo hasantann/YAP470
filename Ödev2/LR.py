dfc72e0ea2df394889074d3717a08e6fa59d9edd
Hasan Tan <hasan_tn@hotmail.com>
2023-07-18 09:37:52 +0300

    ilk deneme hata araştırılacak

--- Ödev2/LR.py
+++ Ödev2/LR.py
@@ -3,13 +3,13 @@ class LinearRegression:
         self.learning_rate = learning_rate
         self.num_epochs = num_epochs
         self.weight_slope = 0
-        self.intercept = 0
+        self.bias = 0
 
     def normalize(self, feature):
         return (feature - min(feature)) / (max(feature) - min(feature))
 
     def hypothesis(self, x):
-        return self.weight_slope * x + self.intercept
+        return self.weight_slope * x + self.bias
 
     def cost_function(self, predictions, targets):
         return sum((predictions - targets) ** 2) / (2 * len(targets))
@@ -22,10 +22,10 @@ class LinearRegression:
             predictions = [self.hypothesis(x) for x in normalized_height]
 
             weight_gradient = sum((predictions[i] - bmi[i]) * normalized_height[i] for i in range(len(height))) / len(height)
-            intercept_gradient = sum(predictions[i] - bmi[i] for i in range(len(height))) / len(height)
+            bias_gradient = sum(predictions[i] - bmi[i] for i in range(len(height))) / len(height)
 
             self.weight_slope -= self.learning_rate * weight_gradient
-            self.intercept -= self.learning_rate * intercept_gradient
+            self.bias -= self.learning_rate * bias_gradient
 
             cost = self.cost_function(predictions, bmi)
 
